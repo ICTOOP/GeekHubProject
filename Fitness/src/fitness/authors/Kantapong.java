6b9e27b657dc002de1be191d081095748162e087
Administrator <Administrator@10.240.9.48>
2014-07-02 09:54:13 +0700

    kantapng

---
+++ Fitness/src/fitness/authors/Kantapong.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fitness.authors;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Kantapong {
+    
+            String name ="Kantapong Rakpong";
+            String stuid ="55024400";
+}
