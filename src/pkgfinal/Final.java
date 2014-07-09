775d496b09176765a736553eceaddcc24cbf9c14
chickenlittle36 <on_sky36@hotmail.co.th>
2014-07-09 08:51:44 +0700

    Finallll

---
+++ src/pkgfinal/Final.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkgfinal;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Final {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+ String[] a = {"10", "10.5", "abc"};
+
+ int i = Integer.parseInt(a[0]);
+
+ i++;
+
+ System.out.println(i);
+
+ 
+    }
+    
+}
